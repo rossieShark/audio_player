8ceaf8fedeb66af25c61b11e214c113f1473717b
Roza Sharkhmullina <rossieshark@gmail.com>
2023-12-01 23:02:35 +0300

    code refactoring

--- lib/ui/widgets/screens/log_pages/widgets/sign_methods.dart
+++ lib/ui/widgets/screens/log_pages/widgets/sign_methods.dart
@@ -13,38 +13,69 @@ class SignMethods extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          GestureDetector(
-            onTap: onGoogleTap,
-            child: SizedBox(
-                width: 18,
-                height: 18,
-                child: Image.asset(
-                  AppImages.google,
-                  fit: BoxFit.cover,
-                )),
-          ),
+          _GoogleSign(onGoogleTap: onGoogleTap),
           const SizedBox(
             width: 7,
           ),
-          SizedBox(
-              width: 20,
-              height: 20,
-              child: Image.asset(
-                AppImages.facebook,
-                fit: BoxFit.cover,
-              )),
+          const _FacebookSign(),
           const SizedBox(
             width: 5,
           ),
-          SizedBox(
-            width: 20,
-            height: 20,
-            child: Image.asset(
-              AppImages.facebook,
-              color: AppColors.white.color,
-              fit: BoxFit.cover,
-            ),
-          ),
+          const _AppleSign(),
         ]);
+  }
+}
+
+class _AppleSign extends StatelessWidget {
+  const _AppleSign();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 20,
+      height: 20,
+      child: Image.asset(
+        AppImages.facebook,
+        color: AppColors.white.color,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
+
+class _FacebookSign extends StatelessWidget {
+  const _FacebookSign();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: 20,
+        height: 20,
+        child: Image.asset(
+          AppImages.facebook,
+          fit: BoxFit.cover,
+        ));
+  }
+}
+
+class _GoogleSign extends StatelessWidget {
+  const _GoogleSign({
+    required this.onGoogleTap,
+  });
+
+  final VoidCallback onGoogleTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onGoogleTap,
+      child: SizedBox(
+          width: 18,
+          height: 18,
+          child: Image.asset(
+            AppImages.google,
+            fit: BoxFit.cover,
+          )),
+    );
   }
 }
