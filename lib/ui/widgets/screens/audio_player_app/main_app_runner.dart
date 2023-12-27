9ce35a3b55383491381e38e7c545c4bb11f0aaad
Roza Sharkhmullina <rossieshark@gmail.com>
2023-12-27 16:33:19 +0300

    add di

--- lib/ui/widgets/screens/audio_player_app/main_app_runner.dart
+++ lib/ui/widgets/screens/audio_player_app/main_app_runner.dart
@@ -1,12 +1,17 @@
-import 'package:audio_player/domain/interfaces/app_builder.dart';
-import 'package:audio_player/domain/interfaces/app_runner.dart';
-import 'package:audio_player/domain/services/get_it_dependencies/get_it_dependencies.dart';
-import 'package:audio_player/firebase_options.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:audio_player/di/init_di.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:logging/logging.dart';
 
+import 'package:audio_player/domain/interfaces/app_builder.dart';
+import 'package:audio_player/domain/interfaces/app_runner.dart';
+import 'package:audio_player/domain/services/get_it_dependencies/get_it_dependencies.dart';
+import 'package:audio_player/firebase_options.dart';
+
 class MainAppRunner implements AppRunner {
+  const MainAppRunner();
+
   @override
   Future<void> preloadData() async {
     WidgetsFlutterBinding.ensureInitialized();
@@ -15,6 +20,7 @@ class MainAppRunner implements AppRunner {
       // ignore: avoid_print
       print('${rec.level.name}: ${rec.time}: ${rec.message}');
     });
+    configureDependencies();
     _initialiseMain();
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
