b09bce5c2ddb2f86969aa4cf5dfe76b7177a81e5
Roza Sharkhmullina <rossieshark@gmail.com>
2024-01-06 20:42:46 +0300

    init_di changes

--- lib/app/di/init_di.config.dart
+++ lib/app/di/init_di.config.dart
@@ -12,64 +12,60 @@ import 'package:audioplayers/audioplayers.dart' as _i4;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../../app/app_logic/blocs/album_detail_bloc/album_detail_bloc.dart'
-    as _i37;
-import '../../app/app_logic/blocs/detail_music_bloc/detail_music_bloc.dart'
-    as _i28;
-import '../../app/app_logic/blocs/favourites_bloc/favourite_album_bloc/favourites_album_bloc.dart'
+import '../app_logic/blocs/album_detail_bloc/album_detail_bloc.dart' as _i37;
+import '../app_logic/blocs/detail_music_bloc/detail_music_bloc.dart' as _i28;
+import '../app_logic/blocs/favourites_bloc/favourite_album_bloc/favourites_album_bloc.dart'
     as _i29;
-import '../../app/app_logic/blocs/favourites_bloc/favourite_song_bloc/favourites_song_bloc.dart'
+import '../app_logic/blocs/favourites_bloc/favourite_song_bloc/favourites_song_bloc.dart'
     as _i31;
-import '../../app/app_logic/blocs/filter_bloc.dart' as _i22;
-import '../../app/app_logic/blocs/genres_bloc/genres_bloc.dart' as _i13;
-import '../../app/app_logic/blocs/home_screen_bloc/album_bloc/album_bloc.dart'
-    as _i27;
-import '../../app/app_logic/blocs/home_screen_bloc/favourite_artist_bloc/favourite_artist_bloc.dart'
+import '../app_logic/blocs/filter_bloc.dart' as _i22;
+import '../app_logic/blocs/genres_bloc/genres_bloc.dart' as _i13;
+import '../app_logic/blocs/home_screen_bloc/album_bloc/album_bloc.dart' as _i27;
+import '../app_logic/blocs/home_screen_bloc/favourite_artist_bloc/favourite_artist_bloc.dart'
     as _i30;
-import '../../app/app_logic/blocs/home_screen_bloc/recently_played_bloc/recently_played_bloc.dart'
+import '../app_logic/blocs/home_screen_bloc/recently_played_bloc/recently_played_bloc.dart'
     as _i35;
-import '../../app/app_logic/blocs/image_bloc/image_bloc.dart' as _i32;
-import '../../app/app_logic/blocs/language_bloc/language_bloc.dart' as _i33;
-import '../../app/app_logic/blocs/music_bloc/music_bloc.dart' as _i17;
-import '../../app/app_logic/blocs/new_playlist_bloc/my_music_foldder_bloc.dart'
+import '../app_logic/blocs/image_bloc/image_bloc.dart' as _i32;
+import '../app_logic/blocs/language_bloc/language_bloc.dart' as _i33;
+import '../app_logic/blocs/music_bloc/music_bloc.dart' as _i17;
+import '../app_logic/blocs/new_playlist_bloc/my_music_foldder_bloc.dart'
     as _i34;
-import '../../app/app_logic/blocs/passwords_bloc/passwords_bloc.dart' as _i19;
-import '../../app/app_logic/blocs/recently_searched_bloc/recently_searched_bloc.dart'
+import '../app_logic/blocs/passwords_bloc/passwords_bloc.dart' as _i19;
+import '../app_logic/blocs/recently_searched_bloc/recently_searched_bloc.dart'
     as _i36;
-import '../../app/app_logic/blocs/search_screen_bloc/search_screen_bloc.dart'
-    as _i38;
-import '../../app/app_logic/blocs/tab_bar_bloc/tab_bar_bloc.dart' as _i25;
-import '../../app/databases/app_database/database.dart' as _i3;
-import '../../app/databases/language_storage/language_storage.dart' as _i16;
-import '../../app/domain/repositories/album_details_repository/album_details_repository.dart'
+import '../app_logic/blocs/search_screen_bloc/search_screen_bloc.dart' as _i38;
+import '../app_logic/blocs/tab_bar_bloc/tab_bar_bloc.dart' as _i25;
+import '../databases/app_database/database.dart' as _i3;
+import '../databases/language_storage/language_storage.dart' as _i16;
+import '../domain/repositories/album_details_repository/album_details_repository.dart'
     as _i26;
-import '../../app/domain/repositories/favourites_repository.dart/favourite_album_repository.dart'
+import '../domain/repositories/favourites_repository.dart/favourite_album_repository.dart'
     as _i11;
-import '../../app/domain/repositories/favourites_repository.dart/favourite_song_repository.dart'
+import '../domain/repositories/favourites_repository.dart/favourite_song_repository.dart'
     as _i10;
-import '../../app/domain/repositories/favourites_repository.dart/favourites_repo.dart'
+import '../domain/repositories/favourites_repository.dart/favourites_repo.dart'
     as _i9;
-import '../../app/domain/repositories/genres_repository/genres_repository.dart'
+import '../domain/repositories/genres_repository/genres_repository.dart'
     as _i12;
-import '../../app/domain/repositories/home_screen_repositories/best_album_repo.dart'
+import '../domain/repositories/home_screen_repositories/best_album_repo.dart'
     as _i6;
-import '../../app/domain/repositories/home_screen_repositories/favourite_artist_repo.dart'
+import '../domain/repositories/home_screen_repositories/favourite_artist_repo.dart'
     as _i8;
-import '../../app/domain/repositories/home_screen_repositories/recently_played_repo.dart'
+import '../domain/repositories/home_screen_repositories/recently_played_repo.dart'
     as _i20;
-import '../../app/domain/repositories/index.dart' as _i14;
-import '../../app/domain/repositories/new_playlist_repository/new_playlist_repository.dart'
+import '../domain/repositories/index.dart' as _i14;
+import '../domain/repositories/new_playlist_repository/new_playlist_repository.dart'
     as _i18;
-import '../../app/domain/repositories/recently_searched_repository/recently_searched_repository.dart'
+import '../domain/repositories/recently_searched_repository/recently_searched_repository.dart'
     as _i21;
-import '../../app/domain/repositories/search_result_repository/search_result_repo.dart'
+import '../domain/repositories/search_result_repository/search_result_repo.dart'
     as _i23;
-import '../../app/domain/repositories/song_detail_repository/song_detail_repository.dart'
+import '../domain/repositories/song_detail_repository/song_detail_repository.dart'
     as _i24;
-import '../../app/domain/services/api_service/service.dart' as _i5;
-import '../../app/domain/services/image_picker_service/image_picker_service.dart'
+import '../domain/services/api_service/service.dart' as _i5;
+import '../domain/services/image_picker_service/image_picker_service.dart'
     as _i15;
-import '../../app/domain/services/services.dart' as _i7;
+import '../domain/services/services.dart' as _i7;
 import 'injectable_module.dart' as _i39;
 
 extension GetItInjectableX on _i1.GetIt {
