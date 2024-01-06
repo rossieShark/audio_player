c8fa991333fd764a6690e48f6c3e48dfecbd82e8
Roza Sharkhmullina <rossieshark@gmail.com>
2024-01-06 20:43:22 +0300

    create custom_button_blob_widget

---
+++ lib/app/ui/widgets/widgets/custom_buttons/play_button/blob_widget.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Blob extends StatelessWidget {
+  final double rotation;
+  final double scale;
+  final Color color;
+
+  const Blob({required this.color, this.rotation = 0, this.scale = 1});
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.scale(
+      scale: scale,
+      child: Transform.rotate(
+        angle: rotation,
+        child: Container(
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(150),
+              topRight: Radius.circular(240),
+              bottomLeft: Radius.circular(220),
+              bottomRight: Radius.circular(180),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
